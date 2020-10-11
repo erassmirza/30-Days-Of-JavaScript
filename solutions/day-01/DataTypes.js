3ca562dd8a374a97d530676c6fd0a33218082cb7
erassmirza <erass.mirza@gmail.com>
2020-10-11 21:16:04 +0500

    Declaring variables with number values

--- solutions/day-01/DataTypes.js
+++ solutions/day-01/DataTypes.js
@@ -14,3 +14,10 @@ let isMarried = true
 
 console.log(firstName, lastName, country, city, age, isMarried)
 
+// Declaring variables with number values
+const gravity = 9.81 // earth gravity  in m/s2
+const boilingPoint = 100 // water boiling point, temperature in oC
+const PI = 3.14 // geometrical constant
+
+console.log(gravity, boilingPoint, PI)
+

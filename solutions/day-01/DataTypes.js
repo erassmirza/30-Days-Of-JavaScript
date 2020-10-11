5524590617332416d1e019d92c7029cdaa858117
erassmirza <erass.mirza@gmail.com>
2020-10-11 21:12:41 +0500

    Declaring different variables of different data types

--- solutions/day-01/DataTypes.js
+++ solutions/day-01/DataTypes.js
@@ -3,3 +3,14 @@ console.log(typeof 5) // number
 console.log(typeof true) // boolean
 console.log(typeof null) // object type
 console.log(typeof undefined) // undefined
+
+// Declaring different variables of different data types
+let firstName = 'Asabeneh' // first name of a person
+let lastName = 'Yetayeh' // last name of a person
+let country = 'Finland' // country
+let city = 'Helsinki' // capital city
+let age = 100 // age in years
+let isMarried = true
+
+console.log(firstName, lastName, country, city, age, isMarried)
+

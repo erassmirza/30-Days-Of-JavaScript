a9d99c27161aedf59ca29ef9b03c9378a84eff3f
erassmirza <erass.mirza@gmail.com>
2020-10-12 17:21:54 +0500

    Level 2 complete

---
+++ solutions/day-02/Level2.js
@@ -0,0 +1,35 @@
+// 1
+console.log('The quote \'There is no exercise better for the heart than reaching down and lifting people up.\' by John Holmes teaches us to help one another.')
+
+// 2
+console.log('\"Love is not patronizing and charity isn\'t about pity, it is about love. Charity and love are the same -- with charity you give love, so don\'t just give money but reach out your hand instead.\"')
+
+// 3
+console.log(+'10' == 10)
+
+// 4
+console.log(Math.ceil(parseFloat('9.8')) == 10) 
+
+// 5
+console.log('python'.endsWith('on'))
+console.log('jargon'.endsWith('on'))
+
+// 6
+console.log('I hope this course is not full of jargon'.endsWith('jargon'))
+
+// 7
+console.log(Math.random()*101)
+
+// 8
+console.log(Math.random()*51)+50
+
+// 9
+console.log(Math.random()*256)
+
+// 10
+
+// 11
+console.log(1+'\t'+1+'\t'+1+'\t'+1+'\t'+1+'\n'+2+'\t'+1+'\t'+2+'\t'+4+'\t'+8+'\n'+3+'\t'+1+'\t'+3+'\t'+9+'\t'+27+'\n'+4+'\t'+1+'\t'+4+'\t'+16+'\t'+64+'\n'+5+'\t'+1+'\t'+5+'\t'+25+'\t'+125)
+
+// 12
+console.log('You cannot end a sentence with because because because is a conjunction'.substr(31,23))

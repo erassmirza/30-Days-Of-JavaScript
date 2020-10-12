2ca55abe3b1a5d7c50126f810ed3e5e245841d12
erassmirza <erass.mirza@gmail.com>
2020-10-12 20:36:47 +0500

    Level 3

---
+++ solutions/day-02/Level3.js
@@ -0,0 +1,7 @@
+// 1
+console.log(('Love is the best thing in this world. Some found their love and some are still looking for their love.'.match(/love/gi)).length)
+
+// 2
+console.log(('You cannot end a sentence with because because because is a conjunction'.match(/because/g)).length)
+
+// 3

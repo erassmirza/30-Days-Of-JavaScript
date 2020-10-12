5a793623c6bd966108ccb6c81b82bda5877c3304
ErassMirza <erass.mirza@gmail.com>
2020-10-12 21:41:30 +0500

    Level 3 complete

--- solutions/day-02/Level3.js
+++ solutions/day-02/Level3.js
@@ -5,3 +5,11 @@ console.log(('Love is the best thing in this world. Some found their love and so
 console.log(('You cannot end a sentence with because because because is a conjunction'.match(/because/g)).length)
 
 // 3
+const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+console.log(sentence.replace(/[%$@&#;!]/g,''))
+
+// 4
+let sen = 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.',
+extract = sen.match(/\d+/g),
+aninc = parseInt(extract[0]*12)+parseInt(extract[1])+parseInt(extract[2]*12)
+console.log(`Total annual income is ${aninc} euro`)
